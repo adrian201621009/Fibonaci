c29694d7aab4362c124268cd2b6dfaab907e2bc2
adrian hernandez <fortaleza2012@hotmail.com>
2017-11-13 19:25:09 -0600

    Agregar archivos de proyecto.

---
+++ Fibonaci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1 = 1, n2 = 1, n3, cont;
+
+            for (cont = 1; cont <5; cont++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3);
+                n1 = n2;
+                n2 = n3;
+                Console.WriteLine(" -- " + (n2 / n1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
